3761ed0882289a6bd5b84e89113728f20208b50d
ci4m3k <mateqpl87@gmail.com>
2022-11-07 01:17:24 +0100

    Navigation page (ver. 0.1)

--- DeliveryPersonApp/DeliveryPersonApp/HomePage.xaml.cs
+++ DeliveryPersonApp/DeliveryPersonApp/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeliveryPersonApp.NavPages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace DeliveryPersonApp
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
         }
     }
 }
