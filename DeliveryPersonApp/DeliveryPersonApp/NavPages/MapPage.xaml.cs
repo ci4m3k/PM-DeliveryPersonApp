4ceb9a834a1890b7be6ebd0fa4dc89e995cc383f
ci4m3k <mateqpl87@gmail.com>
2022-11-17 13:37:57 +0100

    [W6] Displaying map and users location

--- DeliveryPersonApp/DeliveryPersonApp/NavPages/MapPage.xaml.cs
+++ DeliveryPersonApp/DeliveryPersonApp/NavPages/MapPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,42 @@ namespace DeliveryPersonApp.NavPages
         public MapPage()
         {
             InitializeComponent();
+        }
+
+         protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GetLocation(); 
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckAndRequestLocationPermission();
+
+            if(status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+
+                locationsMap.IsShowingUser = true;
+            }
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if(status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                // prompt the user to trun on the permission in settings
+                return status;
+            }
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
         }
     }
 }
