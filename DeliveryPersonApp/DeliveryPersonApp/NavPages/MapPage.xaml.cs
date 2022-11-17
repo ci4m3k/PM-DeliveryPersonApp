e8f438159af4ae1d4f3d14379187b3f8265ae9b9
ci4m3k <mateqpl87@gmail.com>
2022-11-17 13:39:51 +0100

    [W6] Handling location changes on map

--- DeliveryPersonApp/DeliveryPersonApp/NavPages/MapPage.xaml.cs
+++ DeliveryPersonApp/DeliveryPersonApp/NavPages/MapPage.xaml.cs
@@ -1,39 +1,70 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Position = Xamarin.Forms.Maps.Position;
 
 namespace DeliveryPersonApp.NavPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        IGeolocator locator = CrossGeolocator.Current;
+
         public MapPage()
         {
             InitializeComponent();
         }
 
-         protected override void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
 
-            GetLocation(); 
+            GetLocation();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            locator.StopListeningAsync();
         }
 
         private async void GetLocation()
         {
             var status = await CheckAndRequestLocationPermission();
 
-            if(status == PermissionStatus.Granted)
+            if (status == PermissionStatus.Granted)
             {
                 var location = await Geolocation.GetLocationAsync();
 
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(new TimeSpan(0, 1, 0), 100);
+
                 locationsMap.IsShowingUser = true;
+
+                CenterMap(location.Latitude, location.Longitude);
             }
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            CenterMap(e.Position.Latitude, e.Position.Longitude);
+        }
+
+        private void CenterMap(double latitude, double longitude)
+        {
+            Position center = new Position(latitude, longitude);
+            MapSpan span = new MapSpan(center, 1, 1);
+
+            locationsMap.MoveToRegion(span);
         }
 
         private async Task<PermissionStatus> CheckAndRequestLocationPermission()
@@ -43,7 +74,7 @@ namespace DeliveryPersonApp.NavPages
             if (status == PermissionStatus.Granted)
                 return status;
 
-            if(status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
             {
                 // prompt the user to trun on the permission in settings
                 return status;
