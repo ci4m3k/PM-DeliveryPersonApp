31199fb01afb20668bd474d39439f166d77e59d8
ci4m3k <mateqpl87@gmail.com>
2022-12-15 13:12:35 +0100

    [W10] Model - parcel database update

--- DeliveryPersonApp/DeliveryPersonApp/Model/Parcel.cs
+++ DeliveryPersonApp/DeliveryPersonApp/Model/Parcel.cs
@@ -10,10 +10,15 @@ namespace DeliveryPersonApp.Model
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        [MaxLength(20)]
+        [MaxLength(40)]
+        public string Name { get; set; }
         public string Number { get; set; }
         [MaxLength(100)]
         public string Description { get; set; }
+        public double Weight { get; set; }
+        public string Size { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
         public string Status { get; set; }
     }
 }
