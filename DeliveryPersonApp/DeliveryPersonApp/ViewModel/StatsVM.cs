621058ef70e272503f87b1982438890204c6a301
ci4m3k <mateqpl87@gmail.com>
2022-12-22 17:21:35 +0100

    [W11] Stats page - parcel database stats (static)

---
+++ DeliveryPersonApp/DeliveryPersonApp/ViewModel/StatsVM.cs
@@ -0,0 +1,44 @@
+﻿using DeliveryPersonApp.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace DeliveryPersonApp.ViewModel
+{
+    public class StatsVM : INotifyPropertyChanged
+    {
+        private int parcelCount;
+        public int ParcelCount
+        {
+            get { return parcelCount; }
+            set
+            {
+                parcelCount = value;
+                OnPropertyChanged("ParcelCount");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public StatsVM()
+        {
+        }
+
+        public void GetParcels()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var parcels = conn.Table<Parcel>();
+
+                ParcelCount = 6;
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- DeliveryPersonApp/DeliveryPersonApp/Views/StatsPage.xaml.cs
+++ DeliveryPersonApp/DeliveryPersonApp/Views/StatsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeliveryPersonApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,19 @@ namespace DeliveryPersonApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StatsPage : ContentPage
     {
+        private StatsVM vm;
         public StatsPage()
         {
             InitializeComponent();
+
+            vm = Resources["vm"] as StatsVM;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            vm.GetParcels();
         }
     }
 }
