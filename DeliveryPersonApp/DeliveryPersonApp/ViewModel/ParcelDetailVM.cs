0e950ba4367eff41c281da2c58597d8ad3c81948
MarcinKozik <marcinkozik@vp.pl>
2023-01-08 04:04:04 +0100

    [W13] Parcel detail page - functionality (deliver parcel command)

--- DeliveryPersonApp/DeliveryPersonApp/ViewModel/ParcelDetailVM.cs
+++ DeliveryPersonApp/DeliveryPersonApp/ViewModel/ParcelDetailVM.cs
@@ -1,13 +1,20 @@
 ﻿using DeliveryPersonApp.Model;
+using DeliveryPersonApp.Views;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace DeliveryPersonApp.ViewModel
 {
     public class ParcelDetailVM : INotifyPropertyChanged
     {
+        public Command DeliverParcelCommand { get; set; }
+
         private Parcel selectedParcel;
         public Parcel SelectedParcel 
         {
@@ -19,10 +26,37 @@ namespace DeliveryPersonApp.ViewModel
             }
         }
 
+        private bool parcelIsReady;
+        public bool ParcelIsReady
+        {
+            get { return true; }
+        }
+
+        public Page CurrentPage { get; set; }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ParcelDetailVM() 
         {
+            DeliverParcelCommand = new Command<bool>(Deliver, CanDeliver);
+        }
+
+        private void Deliver(bool parameter)
+        {
+            SelectedParcel.Status = "zrealizowane";
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Parcel>();
+                conn.Update(SelectedParcel);
+            }
+
+            App.Current.MainPage.Navigation.RemovePage(CurrentPage);
+        }
+
+        private bool CanDeliver(bool parameter)
+        {
+            return parameter;
         }
 
         private void OnPropertyChanged(string propertyName)
