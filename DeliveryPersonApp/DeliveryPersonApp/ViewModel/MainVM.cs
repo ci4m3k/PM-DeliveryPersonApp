8b4d49603431b3bc2258a4926076aa31ce82510b
MarcinKozik <marcinkozik@vp.pl>
2023-01-09 02:47:30 +0100

    [W13] Login page - validation (empty fields and data from database)

--- DeliveryPersonApp/DeliveryPersonApp/ViewModel/MainVM.cs
+++ DeliveryPersonApp/DeliveryPersonApp/ViewModel/MainVM.cs
@@ -1,22 +1,65 @@
 ﻿using DeliveryPersonApp.Model;
+using DeliveryPersonApp.Views;
 using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace DeliveryPersonApp.ViewModel
 {
-    public class MainVM
+    public class MainVM : INotifyPropertyChanged
     {
+        public Command LoginCommand { get; set; }
+
         public ObservableCollection<Account> Accounts { get; set; } 
 
-        public Account SelectedUser { get; set; } 
-        
+        public Account SelectedUser { get; set; }
+
+        public Page CurrentPage { get; set; }
+
+        private string email;
+        public string Email 
+        { 
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string password;
+        public string Password 
+        { 
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private bool loginIsReady;
+        public bool LoginIsReady
+        {
+            get { return true; }
+        }
+
+        private static string dbName = "account_db.sqlite";
+        private static string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        private string fullPath = Path.Combine(folderPath, dbName);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainVM() 
         {
+            LoginCommand = new Command<bool>(Login, CanLogin);
+
             Accounts = new ObservableCollection<Account>();
 
             List<Account> accountList = new List<Account>()
@@ -32,10 +75,6 @@ namespace DeliveryPersonApp.ViewModel
                 }
             };
 
-            string dbName = "account_db.sqlite";
-            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            string fullPath = Path.Combine(folderPath, dbName);
-
             using (SQLiteConnection conn = new SQLiteConnection(fullPath))
             {
                 Accounts.Clear();
@@ -49,11 +88,55 @@ namespace DeliveryPersonApp.ViewModel
                     foreach (var account in accountList)
                     {
                         conn.Insert(account);
+                        Accounts.Add(account);
                     }
                 }
 
                 SelectedUser = conn.Table<Account>().ToList()[0];
             }
-        } 
+        }
+
+        private async void Login(bool parameter)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(Email);
+            bool isPasswordEmpty = string.IsNullOrEmpty(Password);
+
+            bool isDataCorrect = false;
+
+            foreach (var account in Accounts)
+            {
+                if (Email == account.Email && Password == account.Password)
+                {
+                    isDataCorrect = true;
+                    break;
+                }
+            }
+
+            if (!isEmailEmpty && !isPasswordEmpty)
+            {
+                if (isDataCorrect)
+                {
+                    await App.Current.MainPage.Navigation.PushAsync(new HomePage(SelectedUser));
+                    App.Current.MainPage.Navigation.RemovePage(CurrentPage);
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Błąd", "Podany email lub hasło jest błędny!", "Ok");
+                    Password = null;
+                }
+            }
+            else
+                await App.Current.MainPage.DisplayAlert("Błąd", "Nie podano adresu email lub hasła!", "Ok");
+        }
+
+        private bool CanLogin(bool parameter)
+        {
+           return parameter;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- DeliveryPersonApp/DeliveryPersonApp/Views/MainPage.xaml.cs
+++ DeliveryPersonApp/DeliveryPersonApp/Views/MainPage.xaml.cs
@@ -19,27 +19,10 @@ namespace DeliveryPersonApp.Views
             InitializeComponent();
 
             vm = Resources["vm"] as MainVM;
+            vm.CurrentPage = this;
 
             var assembly = typeof(MainPage);
             profileImage.Source = ImageSource.FromResource("DeliveryPersonApp.Assets.Images.logo.png", assembly);
-        }
-
-        private void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
-            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
-
-            if (!isEmailEmpty && !isPasswordEmpty)
-            {
-                Navigation.PushAsync(new HomePage(vm.SelectedUser));
-                Navigation.RemovePage(this);
-            }
-            else
-            {
-                DisplayAlert("Błąd", "Nie podano adresu email lub hasła", "Ok");
-            }
-
-            // walidacja
         }
     }
 }
